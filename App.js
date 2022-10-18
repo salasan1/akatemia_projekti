9c942b9fae0902d51e866391e810e193cdde75c0
mustafabroski <youremail@yourdomain.com>
2022-10-18 12:03:46 +0300

    React-native-material added to project successfully.

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View} from 'react-native';
 import React, { useState } from "react";
-
+import {Button} from "@react-native-material/core"
 const patients = ["patient1", "patient2", "patient3"]
 export default function App() {
   const [buttonIsPressed, setButtonPressed] = React.useState(false)
   const [isEnabled, setIsEnabled] = useState(false);
   return (
     <View style={styles.container}>
+      <Button/>
       <Text>lel</Text>
       <StatusBar style="auto" />
     </View>
